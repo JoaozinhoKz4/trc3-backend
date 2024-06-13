c31e33b6a08330779326a55a2a8fca3088b016d9
João Vitor Lima <lima.joaozinho98@gmail.com>
2024-06-13 06:44:16 -0300

    Fix import

--- src/perguntas/perguntas.module.ts
+++ src/perguntas/perguntas.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PerguntasService } from './perguntas.service';
 import { PerguntasController } from './perguntas.controller';
+import { OpenAIService } from 'src/openai/openai.service';
+import { OpenAIModule } from 'src/openai/openai.module';
 
 @Module({
+  imports:[OpenAIModule],
   controllers: [PerguntasController],
   providers: [PerguntasService]
 })
